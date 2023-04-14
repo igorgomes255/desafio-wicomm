d6a9ed77d3f25b8bebf6dc88ada140d5e95241dc
Igor Gomes <igorgomes255@gmail.com>
2023-04-14 09:23:57 -0300

    feat: adicionando carrousel no card da imagem

--- src/components/CarrouselCardImage/index.tsx
+++ src/components/CarrouselCardImage/index.tsx
@@ -1,3 +1,21 @@
-const CarrouselCardImage = () => {};
+import { Carousel, CarouselItem } from "react-bootstrap";
+import { CardImage } from "../CardImage";
+import { cardImageData } from "../../utils/data";
+
+const CarrouselCardImage = () => {
+  return (
+    <Carousel controls={false}>
+      {cardImageData.map((card) => (
+        <CarouselItem interval={5000}>
+          <CardImage
+            image={card.image}
+            title={card.title}
+            subtitle={card.subtitle}
+          />
+        </CarouselItem>
+      ))}
+    </Carousel>
+  );
+};
 
 export { CarrouselCardImage };
