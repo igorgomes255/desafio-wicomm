fd2b00b7a5122e091f118cc67579fc203dc5a9db
Igor Gomes <igorgomes255@gmail.com>
2023-04-15 22:22:34 -0300

    fix: colocando nome no carousel de produtos

--- src/components/CarrouselProduct/index.tsx
+++ src/components/CarrouselProduct/index.tsx
@@ -5,45 +5,64 @@ import { CardProduct } from "../CardProduct";
 
 const CarrouselProduct = () => {
   return (
-    <Carousel
-      prevIcon={
-        <GrLinkPrevious
-          size={32}
-          style={{ backgroundColor: "#CACACA", marginLeft: -160 }}
-        />
-      }
-      nextIcon={
-        <GrLinkNext
-          size={32}
-          style={{ backgroundColor: "#CACACA", marginRight: -160 }}
-        />
-      }
-    >
-      {cardProduct.map((review, index) => (
-        <CarouselItem interval={10000}>
-          <Stack
-            direction="horizontal"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              gap: "25px",
-              width: "100%",
-            }}
-          >
-            {cardProduct.map((card, index) => (
-              <CardProduct
-                key={index}
-                image={card.image}
-                title={card.title}
-                price={card.price}
-                quotaPrice={card.quotaPrice}
-              />
-            ))}
-          </Stack>
-        </CarouselItem>
-      ))}
-    </Carousel>
+    <>
+      <h1
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontFamily: "Roboto Condensed",
+          fontStyle: "italic",
+          fontWeight: 700,
+          fontSize: "32px",
+          lineHeight: "38px",
+
+          textAlign: "center",
+          textTransform: "uppercase",
+        }}
+      >
+        Lançamentos
+      </h1>
+      <Carousel
+        prevIcon={
+          <GrLinkPrevious
+            size={32}
+            style={{ backgroundColor: "#CACACA", marginLeft: -160 }}
+          />
+        }
+        nextIcon={
+          <GrLinkNext
+            size={32}
+            style={{ backgroundColor: "#CACACA", marginRight: -160 }}
+          />
+        }
+      >
+        {cardProduct.map((review, index) => (
+          <CarouselItem interval={10000}>
+            <Stack
+              direction="horizontal"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "25px",
+                width: "100%",
+              }}
+            >
+              {cardProduct.map((card, index) => (
+                <CardProduct
+                  key={index}
+                  image={card.image}
+                  title={card.title}
+                  price={card.price}
+                  quotaPrice={card.quotaPrice}
+                />
+              ))}
+            </Stack>
+          </CarouselItem>
+        ))}
+      </Carousel>
+    </>
   );
 };
 
